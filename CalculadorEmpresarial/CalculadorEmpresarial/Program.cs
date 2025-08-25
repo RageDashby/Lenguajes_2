0a1e4a36dd5d961ed0d73d745198b735061937db
RageDashby <ashby.olvera2005@gmail.com>
2025-08-25 14:50:04 -0600

    Add files via upload

---
+++ CalculadorEmpresarial/CalculadorEmpresarial/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bienvenido");
+
+Console.WriteLine("Se estipula una semana laboral de 5 dias");
+Console.WriteLine("Si un empleado no cumple con asistencia completa, se le " +
+    "retiraran ambos bonos");
+
+
