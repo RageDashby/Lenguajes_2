7b6e861558e6d0f2db5a2eef7f783a2101bab293
Ashby Olvera <ashby.olvera2005@gmail.com>
2025-08-25 14:29:05 -0600

    [Agrega] Archivos base del proyecto

---
+++ CalculadorEmpresarial/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadorEmpresarial
+{
+    public interface Empleado
+    {
+        double SueldoBase {  get; set; }
+        Boolean Bono {  get; set; }
+        int DiasTrabajados { get; set; }
+        
+
+
+    
+    }
+}
---
+++ CalculadorEmpresarial/Tecnico.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadorEmpresarial
+{
+    public class Tecnico: Empleado
+    {
+    }
+}
